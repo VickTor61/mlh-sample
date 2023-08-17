fbc3cfe1aee361f16b2e4aa50cc9cc1c5d8e3cef
Enoma Victor Osasenaga <enomavictor@Enomas-MacBook-Pro.local>
2023-08-17 17:22:25 +0100

    Remove unused imports

--- index.ts
+++ index.ts
@@ -1,11 +1,8 @@
-import "dotenv/config";
 import appServer from "http";
 import config from "./config/config";
 import app from "./app";
 
 const server = appServer.createServer(app);
-
-// const port = process.env.PORT;
 
 server.listen(config.port, () => {
   console.log(`⚡️[server]: Server is running on port ${config.port}`);
