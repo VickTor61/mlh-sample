8a4e7fc6786f06cdb627997f98dd18b364780ba5
Enoma Victor Osasenaga <vickjunior2002@gmail.com>
2023-08-28 22:35:06 +0100

    Add cors

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./routes/v1";
 
@@ -7,6 +8,9 @@ const app = express();
 // Parse json request body
 app.use(express.json());
 app.use(cookieParser());
+
+// use cors to allow cross origin server resource sharing
+app.use(cors());
 
 // v1 of Routes
 app.use("/v1", routes);
